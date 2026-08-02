741f7e7cbdf0f22c4aa9f04060d0e890a4179eba
Milka Borja <milk4borj4o@gmail.com>
2026-08-01 20:33:54 -0500

    corregid

--- routes/pedidoRoutes.js
+++ routes/pedidoRoutes.js
@@ -1,6 +1,6 @@
 const express= require(`express`);
 const router = express.Router();
-const pedidoController = require(`../controller/pedidoController`);
+const pedidoController = require(`../controllers/pedidoController`);
 
 router.post(`/pedido`, pedidoController.pedido);
 router.get(`/historial`,pedidoController.obtenerHistorial);
